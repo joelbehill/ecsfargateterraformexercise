db607063d8935fbe661b95385bb53e3c2e94bce9
Joel Hill <joel.hill@gmail.com>
2020-12-29 17:54:06 -0600

    Updating the service to force a new deployment

--- infrastructure/production/ecs.tf
+++ infrastructure/production/ecs.tf
@@ -72,7 +72,7 @@ resource "aws_ecs_service" "main_service" {
   cluster         = aws_ecs_cluster.casechek_cluster.id             # Referencing our created Cluster
   task_definition = aws_ecs_task_definition.main_task.arn # Referencing the task our service will spin up
   launch_type     = "FARGATE"
-  desired_count   = 3 # Setting the number of containers we want deployed to 3
+  desired_count   = 3
 
   load_balancer {
     target_group_arn = aws_lb_target_group.target_group.arn # Referencing our target group
