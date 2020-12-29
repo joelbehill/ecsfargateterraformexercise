ffce79ff791b027c225fb92e087ab942a5f09dff
Joel Hill <joel.hill@gmail.com>
2020-12-29 13:07:19 -0600

    Using Terraform to create ECS cluster / task / service.  Need to add ECR.  Not working currently because tg issue

---
+++ infrastructure/production/ecs.tf
@@ -0,0 +1,69 @@
+resource "aws_ecs_cluster" "casechek_cluster" {
+  name = "casechek" # Naming the cluster
+}
+
+resource "aws_ecs_task_definition" "main_task" {
+  family                   = "main-task" # Naming our first task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "main-task",
+      "image": "730061254062.dkr.ecr.us-east-2.amazonaws.com/express:latest",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 8080,
+          "hostPort": 8080
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "main_service" {
+  name            = "main-service"                             # Naming our first service
+  cluster         = aws_ecs_cluster.casechek_cluster.id             # Referencing our created Cluster
+  task_definition = aws_ecs_task_definition.main_task.arn # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 3 # Setting the number of containers we want deployed to 3
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn # Referencing our target group
+    container_name   = aws_ecs_task_definition.main_task.family
+    container_port   = 8080 # Specifying the container port
+  }
+
+  network_configuration {
+    subnets          = [aws_default_subnet.default_subnet_a.id, aws_default_subnet.default_subnet_b.id, aws_default_subnet.default_subnet_c.id]
+    assign_public_ip = true # Providing our containers with public IPs
+  }
+}
--- infrastructure/production/vpc.tf
+++ infrastructure/production/vpc.tf
@@ -25,3 +25,19 @@ resource "aws_eip" "nat" {
   vpc = true
 }
 
+# For allowing us to create a service in the default VPC
+
+resource "aws_default_vpc" "default_vpc" {
+}
+
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "us-east-2a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "us-east-2b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "us-east-2c"
+}
